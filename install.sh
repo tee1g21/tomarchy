ec47826ae976db58a7579dd83ef1bdbf529c699b
Tom Evans <tomevans1517@gmail.com>
2025-12-17 16:10:10 +0000

    Very minor case change

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ mkdir -p ~/.local/bin
 # Link custom scripts
 #    -sf (symbolic link, force overwrite if it exists)
 # Custom Menu Script which include suspend
-ln -sf ~/Repositories/Tomarchy/.local/bin/omarchy-menu-custom ~/.local/bin/omarchy-menu-custom
+ln -sf ~/Repositories/tomarchy/.local/bin/omarchy-menu-custom ~/.local/bin/omarchy-menu-custom
 
 # 3. Make them executable (just in case)
-chmod +x ~/Repositories/Tomarchy/.local/bin/*
+chmod +x ~/Repositories/tomarchy/.local/bin/*
 
 echo "Tomarchy installed! 🍅"
